5a21074ab927803b1ccbc1a69b78582bade89a75
carol <lcvanega@uamv.edu.ni>
2022-10-25 21:29:37 -0600

    Relacion Persona-Usuario

---
+++ src/main/java/com/uam/eCommerceUltimate/repository/IUsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.uam.eCommerceUltimate.repository;
+
+import com.uam.eCommerceUltimate.model.UsuarioWeb;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IUsuarioRepository extends JpaRepository<UsuarioWeb,Integer>
+{
+}
